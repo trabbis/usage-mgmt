158a4294c45c0dc76fa84a53fc038c207d8230a5
Duksun Choi <duksun.choi@telus.com>
2020-08-24 15:39:34 -0400

    adding parameters

--- src/main/java/com/telus/usage/mgmt/RawUsageController.java
+++ src/main/java/com/telus/usage/mgmt/RawUsageController.java
@@ -4,13 +4,17 @@ package com.telus.usage.mgmt;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.telus.usage.mgmt.beans.RawUsageListResponseVO;
+import com.telus.usage.mgmt.beans.SearchRawUsageListVO;
 import com.telus.usage.mgmt.beans.TeamMember;
 import com.telus.usage.mgmt.repository.JdbcRepository;
 
@@ -27,19 +31,15 @@ public class RawUsageController {
 		return new ResponseEntity<>(version, HttpStatus.OK);
 	}
 
-//	@GetMapping("/teamMember")
-//	public @ResponseBody ResponseEntity<Object> getTeamMember() {
-//
-//		//String version = "Welcome...";
-//		
-//		List<TeamMember> lists = jdbcRepository.findByNameAndPrice("T010050");
-//		
-//		return new ResponseEntity<>(lists, HttpStatus.OK);
-//	}
-	
 	@GetMapping("/getRawUsageList")
-	public @ResponseBody ResponseEntity<Object> getRawUsageList() {
+	public @ResponseBody ResponseEntity<Object> getRawUsageList(
+			@RequestParam(value = "phoneNumber", required = true) String phoneNumber,
+			@RequestParam(value = "serviceType", required = true) String serviceType,
+			@RequestParam(value = "fromDate", required = true)  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) String fromDate) {
 
+		
+		SearchRawUsageListVO searchRawUsageListVO = new SearchRawUsageListVO();
+		
 		RawUsageListResponseVO lists = jdbcRepository.getRawUsageList("WISP");
 		
 		return new ResponseEntity<>(lists, HttpStatus.OK);
