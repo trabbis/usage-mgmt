9f4ccbfef48b3405fccb9adf6c151a9b68ab9157
Duksun Choi <duksun.choi@telus.com>
2020-08-31 11:12:17 -0400

    adding input param validation

--- src/main/java/com/telus/usage/mgmt/RawUsageController.java
+++ src/main/java/com/telus/usage/mgmt/RawUsageController.java
@@ -1,12 +1,19 @@
 package com.telus.usage.mgmt;
 
 
+import java.time.LocalDate;
+
+import javax.validation.ValidationException;
+import javax.validation.constraints.Size;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +24,7 @@ import com.telus.usage.mgmt.beans.SearchRawUsageListVO;
 import com.telus.usage.mgmt.beans.SearchRawUsageVO;
 import com.telus.usage.mgmt.response.RawUsageListResponse;
 import com.telus.usage.mgmt.services.RawUsageService;
+
 
 @RestController
 public class RawUsageController {
@@ -47,32 +55,26 @@ public class RawUsageController {
 	public @ResponseBody ResponseEntity<Object> getRawUsageList(
 			@RequestParam(value = "phoneNumber", required = true) String phoneNumber,
 			@RequestParam(value = "serviceType", required = true) String serviceType,
-			@RequestParam(value = "fromDate", required = true)  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) String fromDate,
-			@RequestParam(value = "toDate", required = true)  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) String toDate) {
+			@RequestParam(value = "batchNumber", required = true) Integer batchNumber,
+			@RequestParam(value = "fromDate", required = true)  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fromDate,
+			@RequestParam(value = "toDate", required = true)  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate toDate) {
 
+		
+		if (StringUtils.length(phoneNumber) < 10) {
+			throw new ValidationException("Phone number should be at least 10 in length");
+		}
 		
 		SearchRawUsageListVO searchRawUsageListVO = new SearchRawUsageListVO();
 		SearchRawUsageVO searchRawUsageVO = new SearchRawUsageVO();
-		searchRawUsageVO.setFromDate(fromDate);
-		searchRawUsageVO.setToDate(toDate);
-		searchRawUsageVO.setPhoneNumber(phoneNumber);
+		searchRawUsageVO.setFromDate(fromDate.toString());
+		searchRawUsageVO.setToDate(toDate.toString());
+		searchRawUsageVO.setPhoneNumber("1" + phoneNumber);
 		searchRawUsageVO.setServiceType(serviceType);
 		
 		searchRawUsageListVO.setSearchRawUsage(searchRawUsageVO);
 		
 		RawUsageListResponse response = rawUsageService.getRawUsageList(searchRawUsageListVO);
 
-		//TODO custom ExceptionHandler
-//		try {
-//			response = Convertor.convert(lists);
-//		} catch (Exception e) {
-//			log.error(e.toString());
-//			MultiValueMap<String, String> headers =  new LinkedMultiValueMap<>();
-//			headers.add("service exception", e.getMessage());
-//			return new ResponseEntity<>(e.getMessage(), headers, HttpStatus.INTERNAL_SERVER_ERROR);
-//		}
-		
-		
 		
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
--- src/main/java/com/telus/usage/mgmt/exceptions/ValidationException.java
+++ src/main/java/com/telus/usage/mgmt/exceptions/ValidationException.java
@@ -4,10 +4,8 @@ public class ValidationException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 	
-    public ValidationException(String errorMessage, Throwable err) {
-        super(errorMessage, err);
+    public ValidationException(String errorMessage) {
+        super(errorMessage);
     }
-
-	
 	
 }
