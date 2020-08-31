ed79b1f94e9e8852240d64ab08ee38480180c018
Duksun Choi <duksun.choi@telus.com>
2020-08-31 16:30:28 -0400

    phoneNumber validation

--- src/main/java/com/telus/usage/mgmt/util/Convertor.java
+++ src/main/java/com/telus/usage/mgmt/util/Convertor.java
@@ -29,10 +29,10 @@ public class Convertor {
 	
 	public static void validateInputParameters(SearchRawUsageVO searchRawUsageVO) {
 		
-		searchRawUsageVO.setPhoneNumber("1" + searchRawUsageVO.getPhoneNumber());
-		if (StringUtils.length(searchRawUsageVO.getPhoneNumber()) < 10) {
-			throw new ValidationException("Phone number should be at least 10 in length");
+		if (StringUtils.length(searchRawUsageVO.getPhoneNumber()) != 10) {
+			throw new ValidationException("Invalid Phone number");
 		}
+		searchRawUsageVO.setPhoneNumber("1" + searchRawUsageVO.getPhoneNumber());
 		
 	}	
 
